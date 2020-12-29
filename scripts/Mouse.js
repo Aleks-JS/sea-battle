fa77161048294fb1db558ee429b072926c779fb4
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-29 09:56:28 +0300

    described the Mouse class

--- scripts/Mouse.js
+++ scripts/Mouse.js
@@ -1,1 +1,69 @@
-class Mouse {}
+class Mouse {
+  element = null;
+
+  under = false; // flag
+  prevUnder = false;
+
+  curX = null; // current coordinates
+  curY = null;
+
+  prevX = null; // previous coordinates
+  prevY = null;
+
+  curLeftBtn = false; // left mouse button state
+  prevLeftBtn = false;
+
+  delta = 0; // mouse wheel scroll state
+  prevDelta = 0;
+
+  constructor(element) {
+    this.element = element;
+
+    // mouse event handlers
+    element.addEventListener('mousemove', (e) => {
+      this.tick();
+
+      this.curX = e.clientX;
+      this.curY = e.clientY;
+    });
+    element.addEventListener('mouseenter', (e) => {
+      this.tick();
+
+      this.curX = e.clientX;
+      this.curY = e.clientY;
+    });
+    element.addEventListener('mouseleave', (e) => {
+      this.tick();
+
+      this.curX = e.clientX;
+      this.curY = e.clientY;
+    });
+    element.addEventListener('mousedown', (e) => {
+      this.tick();
+
+      this.curX = e.clientX;
+      this.curY = e.clientY;
+    });
+    element.addEventListener('mouseup', (e) => {
+      this.tick();
+
+      this.curX = e.clientX;
+      this.curY = e.clientY;
+    });
+    element.addEventListener('wheel', (e) => {
+      this.tick();
+
+      this.curX = e.clientX;
+      this.curY = e.clientY;
+    });
+  }
+
+  // a method that will write the current state to the previous one
+  tick() {
+    this.prevX = this.curX;
+    this.prevY = this.curY;
+    this.prevUnder = this.under;
+    this.prevLeftBtn = this.curLeftBtn;
+    this.prevDelta = this.delta;
+  }
+}
