d7036ab25e5a89dad64a1426e5c5c05bf01f198a
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-12-29 11:14:03 +0300

    added parameters to ShipView class

--- scripts/ShipView.js
+++ scripts/ShipView.js
@@ -1,1 +1,10 @@
-class ShipView extends Ship {}
+class ShipView extends Ship {
+  div = null;
+
+  constructor(size, direction) {
+    super(size, direction);
+
+    const div = document.createElement('div');
+    div.classList.add('ship');
+  }
+}
