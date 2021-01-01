851dc8e721dc4b602ea87be0f1fcde2676836f38
Aleksey Kresnikov <kresnikov@yandex.ru>
2021-01-02 00:43:21 +0300

    create class App

--- scripts/App.js
+++ scripts/App.js
@@ -1,1 +1,14 @@
-class App {}
+class App {
+  player = null;
+  opponent = null;
+
+  constructor() {
+    const player = new BattleFieldView();
+    const opponent = new BattleFieldView();
+
+    Object.assign(this, { player, opponent });
+
+    document.querySelector('[data-side="player"]').append(player.root);
+    document.querySelector('[data-side="opponent"]').append(opponent.root);
+  }
+}
