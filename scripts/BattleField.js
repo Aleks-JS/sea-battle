b32df0155271fbba39ebc0ed3f3447e93733aa03
Aleksey Kresnikov <kresnikov@yandex.ru>
2021-01-03 02:24:46 +0300

    BattleField: get matrix implemented a method for checking a free and occupied cell in the playing field

--- scripts/BattleField.js
+++ scripts/BattleField.js
@@ -56,8 +56,8 @@ class BattleField {
       // }
 
       // optimized
-      for (let y = ship.y - 1; y < ship.y + ship.size * dy + dx; y++) {
-        for (let x = ship.x - 1; x < ship.x + ship.size * dx + dy; x++) {
+      for (let y = ship.y - 1; y < ship.y + ship.size * dy + dx + 1; y++) {
+        for (let x = ship.x - 1; x < ship.x + ship.size * dx + dy + 1; x++) {
           if (this.inField(x, y)) {
             const item = matrix[y][x];
             item.freeCell = false;
